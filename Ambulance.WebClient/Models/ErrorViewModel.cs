ba55c173331f6086e59515afd1484aac49fcd568
Ristoll <chamata.kristina@gmail.com>
2025-10-25 00:31:55 +0300

    Замінила веб-клієнт на Blazor

--- Ambulance.WebClient/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Ambulance.WebClient.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
